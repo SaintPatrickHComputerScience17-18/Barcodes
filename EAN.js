0d4b3a570c4aec431bac71e26752444f06fa3ad5
Mr. Fleming <efleming@stpatrick.org>
2017-12-04 04:46:10 -0600

    Add files via upload

---
+++ EAN.js
@@ -0,0 +1,26 @@
+//require something maybe?
+
+const EAN = function(){
+
+    let sysCode;
+    let manCode;
+    let proCode;
+    let checkDigit;
+
+
+    function makeSysCode(){
+        //create this function
+    }
+
+    function check(){
+        //create this function
+    }
+    
+    function read(){
+        //create this function
+
+    }
+    return {sysCode,manCode,proCode,checkDigit,read};
+};
+
+module.exports = EAN;
---
+++ ISBN.js
@@ -0,0 +1,18 @@
+//require something?
+
+const ISBN = function(){
+
+    let code;
+    let checkDigit;
+
+    function check(){
+        //create this function
+    }
+    function read(){
+        //create this function
+    }
+
+    return {code, checkDigit, read};
+};
+
+module.exprots = ISBN;
