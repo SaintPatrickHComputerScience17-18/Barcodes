f808fbaf507b51b4cb8c202a177d783cf94eb24f
Mr. Fleming <efleming@stpatrick.org>
2017-12-07 17:15:49 -0600

    Add files via upload

--- ISBN.js
+++ ISBN.js
@@ -1,12 +1,23 @@
-//require something?
+const Random = require('./Random.js');
 
 const ISBN = function(){
 
-    let code;
-    let checkDigit;
+    let code = Random().digit(9);
+    let checkDigit = check();
 
     function check(){
-        //create this function
+        let sum = 0;
+        for(let number = 10;number>=2;number--){
+            sum += number*code[0];
+        }
+        let remainder = sum % 11;
+        if(remainder == 0){
+            return "0";
+        }
+        if(remainder ==1){
+            return "X";
+        }
+        return 11-remainder;
     }
     function read(){
         //create this function
@@ -15,4 +26,11 @@ const ISBN = function(){
     return {code, checkDigit, read};
 };
 
-module.exprots = ISBN;
+function test(){
+    let b = new ISBN();
+    console.log(b.code);
+    console.log(b.checkDigit);
+    console.log(b.read());
+}
+//test();
+module.exports = ISBN;
